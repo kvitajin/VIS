ce2a4a9a41a7496f46a1d1755a1cba65f4ef2a6c
Jindřich Kvita <jindrich.kvita@gnj.cz>
2019-12-07 01:28:35 +0100

    obec opravena a otestovana

---
+++ Farabuk/server/src/test/AlbumReporitoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class AlbumReporitoryTest extends TestCase {
+
+    public function testReadAllRearDeep() {
+
+    }
+
+    public function testReadDeep() {
+
+    }
+
+    public function testReadAll() {
+
+    }
+
+    public function testCreate() {
+
+    }
+
+    public function testReadAllDeep() {
+
+    }
+
+    public function testCreateDeep() {
+
+    }
+
+    public function testUpdate() {
+
+    }
+
+    public function testDelete() {
+
+    }
+
+    public function testReadRearDeep() {
+
+    }
+
+    public function testRead() {
+
+    }
+}
--- Farabuk/server/src/test/ObecRepositoryTest.php
+++ Farabuk/server/src/test/ObecRepositoryTest.php
@@ -2,56 +2,64 @@
 require_once __DIR__ . "/../repository/ObecRepository.php";
 
 use PHPUnit\Framework\TestCase;
-use Connection\Connection;
 
 class ObecRepositoryTest extends TestCase {
 
     public function testReadRearDeep() {
+        $this->assertTrue(ObecRepository::readRearDeep(1, 2) != null);
 
     }
 
     public function testReadAll() {
-        var_dump(ObecRepository::readAll());
+//        var_dump(ObecRepository::readAll());
+        $this->assertTrue(ObecRepository::readAll() != null);
     }
 
     public function testCreateDeep() {
-
-    }
-
-    public function testGetTableName() {
-
+        $obec = new Obec();
+        $obec->nazev = "Kotehule";
+        $obec->uri = "kotehule";
+        $obec->erb = "neco.jpg";
+        $obec->viditelna = 1;
+        $this->assertTrue(ObecRepository::createDeep($obec) > 0);
     }
 
     public function testReadDeep() {
-
+        //var_dump(ObecRepository::readDeep(1));
+        $this->assertTrue(ObecRepository::readDeep(1) != null);
     }
 
     public function testReadAllDeep() {
-
+//        var_dump(ObecRepository::readAllDeep());
+        $this->assertTrue(ObecRepository::readAllDeep() != null);
     }
 
     public function testUpdate() {
-
+        $obec = new Obec();
+        $obec->id = 1;
+        $obec->nazev = "Kuncice";
+        $this->assertTrue(ObecRepository::update($obec));
     }
 
     public function testCreate() {
-        $obec= new \Obec();
-        $obec->nazev="Kotehule";
-        $obec->uri="kotehule";
-        $obec->erb= "neco.jpg";
-        $obec->viditelna=1;
-        ObecRepository::create($obec);
+        $obec = new Obec();
+        $obec->nazev = "Kotehule";
+        $obec->uri = "kotehule";
+        $obec->erb = "neco.jpg";
+        $obec->viditelna = 1;
+        $this->assertTrue(ObecRepository::create($obec) > 0);
     }
 
     public function testRead() {
-        assert(ObecRepository::read(1));
+        $this->assertTrue(ObecRepository::read(1) != null);
     }
 
     public function testDelete() {
-
+        $this->assertTrue(ObecRepository::delete(1));
     }
 
     public function testReadAllRearDeep() {
-
+//        var_dump(ObecRepository::readAllRearDeep(2));
+        $this->assertTrue(ObecRepository::readAllRearDeep(2) != null);
     }
 }
