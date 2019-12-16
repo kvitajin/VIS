05e83d9769bd1f80a38db707ac15b8030e1d4ee7
Jindřich Kvita <jindrich.kvita@gnj.cz>
2019-12-16 19:51:03 +0100

    hotové nahrávání fotek

--- Farabuk/server/src/spojova/makeFoto.php
+++ Farabuk/server/src/spojova/makeFoto.php
@@ -18,38 +18,57 @@ if (isset($_SESSION["id"]) && SkupinaUzivatelRepository::jeOpravnen($_SESSION["i
     else{
         $tmp->id = $_POST['album'];
     }
-
-
-
-    //$files = array_filter($_FILES['upload']['name']); //something like that to be used before processing files.
-
-// Count # of uploaded files in array
+    
     $total = count($_FILES['fotky']['name']);
-
+//    echo $total;
 // Loop through each file
+//    print "<pre>";
+//    print_r($_FILES['fotky']['name']);
+//    echo "<br>";
+//    print "</pre>";
     for( $i=0 ; $i < $total ; $i++ ) {
-
+        $tmpPozice=__DIR__ . "/../../../server/db/tmp/".basename($_FILES["fotky"]["name"][$i]);
         //Get the temp file path
         $tmpFilePath = $_FILES['fotky']['tmp_name'][$i];
+        if (isset($_SERVER["CONTENT_LENGTH"]) && (int)$_SERVER["CONTENT_LENGTH"] > (10 * 1024 * 1024)) {
+            throw new Exception("Tento soubor je příliš velký!");
+        }
 
-        //Make sure we have a file path
+
+//        echo $tmpFilePath. "<br>";
         if ($tmpFilePath != ""){
+            $pripona = image_type_to_extension(exif_imagetype($tmpFilePath));
+            $hashName = hash_file("sha256", $_FILES["fotky"]["tmp_name"][$i]);
+            if (!in_array(exif_imagetype($tmpFilePath), [IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_BMP, IMAGETYPE_GIF])) {
+                throw new Exception("Tento formát souboru není námi podporovaný!");
+            }
             //Setup our new file path
-            $newFilePath = __DIR__."/../../db/files/" . $_FILES['fotky']['name'][$i];
-
+            $newFilePath = __DIR__ . "/../../../server/db/files/" . $hashName . $pripona;
             //Upload the file into the temp dir
             if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-
                 //Handle other code here
+                $data= new Foto();
+                $data->datum=date('d.m.Y');
+                $data->nazevSouboru=$hashName.$pripona;
+                $data->ckIdAlbum=$tmp->id;
+                $data->sirka = 0;
+                $data->viditelna=1;
+                FotoRepository::create($data);
 
             }
         }
     }
 
+    /*
+     * /^[a-z0-9-]+$/g
+    urlencode, podle nadpisu, jen pokud neni nastaven uz uri
+    projet regexem (a-z a pomlcka) preg_match
+    unikatnost obec-druh-uri
+    */
 
 
-//    header('Location:/'. $_SESSION['obec']);
-//    die();
+    header('Location:/'. $_SESSION['obec']);
+    die();
 }
 else{
     throw new Exception("uzivatel neni opravnen");
